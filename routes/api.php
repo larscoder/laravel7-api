8d28f259fc42a970f49dd0727c0ad54fa3167598
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-06-02 09:11:34 -0500

    Agregamos el endpoint categories

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('products', 'ProductController');
+Route::apiResource('categories', 'CategoryController');
