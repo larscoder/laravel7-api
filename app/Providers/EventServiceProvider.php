9032ab8281d8aa7f490de72ccaa0d35764a4a159
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-06-09 08:07:33 -0500

    Agregamos  Job poara cuando el usuario inicia sesión se actualice el la columnas last_login

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Events\ModelRated;
+use App\Jobs\UpdateLastLogin;
 use App\Listeners\SendEmailModelRatedNotification;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -22,6 +24,9 @@ class EventServiceProvider extends ServiceProvider
         ],
         ModelRated::class => [
             SendEmailModelRatedNotification::class,
+        ],
+        Login::class => [
+            UpdateLastLogin::class,
         ],
     ];
 
